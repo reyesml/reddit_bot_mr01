9f729c87e16fe834bdf882bd45ca4e55f4048a13
Matthew Reyes <matthewlreyes@gmail.com>
2014-05-18 21:14:02 -0700

    Testing OAuth by submitting a self-post

--- theBot.py
+++ theBot.py
@@ -38,6 +38,7 @@ client = requests.session()
 #request an OAuth token
 r = client.post('https://ssl.reddit.com/api/v1/access_token',headers=headers, auth=client_auth, data=post_data)
 
+#some print statements for debugging
 print('-------Response 2 headers---------\n', r.headers, '\n-------------------')
 j = json.loads(r.text)
 print('---Response 2 Body-----')
@@ -52,5 +53,24 @@ headers["Authorization"] = 'bearer ' + access_token
 #verify that the token works, by requesting our user data using the access_token in the header
 r = client.get("https://oauth.reddit.com/api/v1/me", headers=headers)
 
+#More debugging
 print('-------User Data--------\n', r.json(), '\n------------------')
+
+
+#Let's try making a post to /r/FreeKarma.  You WILL need a little
+#bit of link karma before your bot can post without getting stopped
+#by the captcha
+post_data = {
+				'api_type': 'json',
+				'kind': 'self',
+				'sr' : 'FreeKarma',
+				'title' : 'Hello, world.',
+				'text' : 'Hooray, it works!'
+			}
+
+r = client.post("https://oauth.reddit.com/api/submit", headers=headers, data=post_data)
+
+#More debugging
+print('----------Submission Response----------\n', r.json(), '\n-------------------')
+
 
