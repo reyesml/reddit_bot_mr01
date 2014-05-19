6cd7dafeda527ef58817254fd33c2e4619471cea
Matthew Reyes <matthewlreyes@gmail.com>
2014-05-18 21:37:38 -0700

    wrapped the meat of the script in the runTheBot() method

--- theBot.py
+++ theBot.py
@@ -11,66 +11,63 @@ def get_config():
   config = json.loads(json_data)
   return config
 
-#Retrieve the config data
-config = get_config()
-username = config['username']
-password = config['password']
-client_id = config['Client_ID']
-client_secret = config['Client_Secret']
+def runTheBot():
+	#Retrieve the config data
+	config = get_config()
+	username = config['username']
+	password = config['password']
+	client_id = config['Client_ID']
+	client_secret = config['Client_Secret']
 
-#build the auth data
-client_auth = requests.auth.HTTPBasicAuth(client_id, client_secret)
-
-
-post_data = {
-			  'grant_type' : 'password',
-			  'username': username,
-              'password': password,
-              }
+	#build the auth data
+	client_auth = requests.auth.HTTPBasicAuth(client_id, client_secret)
 
 
-#set the header for all the following requests.  The user-agent should be unique.
-headers = 	{'user-agent': 'My first reddit bot 05182014'}
- 
-#create a requests.session that'll handle our cookies for us
-client = requests.session()
-
-#request an OAuth token
-r = client.post('https://ssl.reddit.com/api/v1/access_token',headers=headers, auth=client_auth, data=post_data)
-
-#some print statements for debugging
-print('-------Response 2 headers---------\n', r.headers, '\n-------------------')
-j = json.loads(r.text)
-print('---Response 2 Body-----')
-print(j)
-
-#store the access_token we receieve from the previous request
-access_token = j['access_token']
-
-#add the access token to our header for future use
-headers["Authorization"] = 'bearer ' + access_token
-
-#verify that the token works, by requesting our user data using the access_token in the header
-r = client.get("https://oauth.reddit.com/api/v1/me", headers=headers)
-
-#More debugging
-print('-------User Data--------\n', r.json(), '\n------------------')
+	post_data = {
+				'grant_type' : 'password',
+				'username': username,
+				'password': password
+				}
 
 
-#Let's try making a post to /r/FreeKarma.  You WILL need a little
-#bit of link karma before your bot can post without getting stopped
-#by the captcha
-post_data = {
+	#set the header for all the following requests.  The user-agent should be unique.
+	headers = 	{'user-agent': 'My first reddit bot 05182014'}
+	 
+	#create a requests.session that'll handle our cookies for us
+	client = requests.session()
+
+	#request an OAuth token
+	r = client.post('https://ssl.reddit.com/api/v1/access_token',headers=headers, auth=client_auth, data=post_data)
+	j = json.loads(r.text)
+	#store the access_token we receieve from the previous request
+	access_token = j['access_token']
+
+	#add the access token to our header for future use
+	headers["Authorization"] = 'bearer ' + access_token
+
+	#verify that the token works, by requesting our user data using the access_token in the header
+	r = client.get("https://oauth.reddit.com/api/v1/me", headers=headers)
+
+	#Some debugging
+	print('-------User Data--------\n', r.json(), '\n------------------')
+
+
+	#Let's try making a post to /r/FreeKarma.  You WILL need a little
+	#bit of link karma before your bot can post without getting stopped
+	#by the captcha
+	post_data = {
 				'api_type': 'json',
 				'kind': 'self',
 				'sr' : 'FreeKarma',
 				'title' : 'Hello, world.',
 				'text' : 'Hooray, it works!'
-			}
+				}
 
-r = client.post("https://oauth.reddit.com/api/submit", headers=headers, data=post_data)
+	r = client.post("https://oauth.reddit.com/api/submit", headers=headers, data=post_data)
 
-#More debugging
-print('----------Submission Response----------\n', r.json(), '\n-------------------')
+	#More debugging
+	print('----------Submission Response----------\n', r.json(), '\n-------------------')
 
 
+if __name__ == '__main__':
+	runTheBot()
