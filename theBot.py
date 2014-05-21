ae5769d4202b4a8922d68a1401c43b9f8fca065c
Matthew Reyes <matthewlreyes@gmail.com>
2014-05-20 22:33:56 -0700

    Moved reddit api callouts into somewhat helpful methods" "

--- theBot.py
+++ theBot.py
@@ -3,13 +3,66 @@ from pprint import pprint
 import requests
 import requests.auth
 import json
+import time
+
+#self-imposed waiting time so we don't get our bot locked
+#out by making too many requests.
+WAIT_TIME = 2
 
 
 def get_config():
-  print('Loading config')
+  print('Loading config...')
   json_data = open('config.json', 'r').read()
   config = json.loads(json_data)
   return config
+
+def getOAuthData(session, userName, password, clientID, clientSecret, headers):
+	print('Requesting OAuth Token for ', userName, '...')
+	data = {
+			'grant_type' : 'password',
+			'username': userName,
+			'password': password
+			}
+	client_auth = requests.auth.HTTPBasicAuth(clientID, clientSecret)
+	r = session.post('https://ssl.reddit.com/api/v1/access_token',headers=headers, auth=client_auth, data=data)
+	print(r.headers)
+	#hold on to your butts, no error checking here...
+	j = json.loads(r.text)
+	time.sleep(WAIT_TIME)
+	return j
+
+
+def createSelfPost(session, headers, subReddit, title, body):
+	print('Submitting self-post to ', subReddit, '...')
+	data =	{
+			'api_type': 'json',
+			'kind': 'self',
+			'sr' : subReddit,
+			'title' : title,
+			'text' : body
+			}
+	r = session.post("https://oauth.reddit.com/api/submit", headers=headers, data=data)
+	print(r.headers)
+	#hold on to your butts, no error checking here...
+	j = json.loads(r.text)
+	time.sleep(WAIT_TIME)
+	return j
+
+def sendPrivateMessage(session, headers, to, subject, body):
+	print('Sending message to ', to, '...')
+	data = {
+			'api_type' : 'json',
+			'subject' : subject,
+			'text' : body,
+			'to' : to
+			}
+	r = session.post('https://oauth.reddit.com/api/compose',headers=headers, data=data)
+	print(r.headers)
+	#hold on to your butts, no error checking here...
+	j = json.loads(r.text)
+	time.sleep(WAIT_TIME)
+	return j
+
 
 def runTheBot():
 	#Retrieve the config data
@@ -18,55 +71,26 @@ def runTheBot():
 	password = config['password']
 	client_id = config['Client_ID']
 	client_secret = config['Client_Secret']
-
-	#build the auth data
-	client_auth = requests.auth.HTTPBasicAuth(client_id, client_secret)
-
-
-	post_data = {
-				'grant_type' : 'password',
-				'username': username,
-				'password': password
-				}
-
-
-	#set the header for all the following requests.  The user-agent should be unique.
 	headers = 	{'user-agent': 'My first reddit bot 05182014'}
-	 
-	#create a requests.session that'll handle our cookies for us
 	client = requests.session()
 
 	#request an OAuth token
-	r = client.post('https://ssl.reddit.com/api/v1/access_token',headers=headers, auth=client_auth, data=post_data)
-	j = json.loads(r.text)
+	#timeTokenRequested = time.time() #our token will expire 1 hour from now.
+	oauthData = getOAuthData(client, username, password, client_id, client_secret, headers)
+	print('Oauth Data: ', oauthData)
 	#store the access_token we receieve from the previous request
-	access_token = j['access_token']
-
+	access_token = oauthData['access_token']
 	#add the access token to our header for future use
 	headers["Authorization"] = 'bearer ' + access_token
+	#refresh_token = oauthData['refresh_token']
+	print('Token 01 ', access_token, ' will expire on: ', oauthData['expires_in'])
+	message = 'The current time is {0}'.format(time.asctime())
+	resp = sendPrivateMessage(client, headers, '<Test USER>', 'hello, again', message)
 
-	#verify that the token works, by requesting our user data using the access_token in the header
-	r = client.get("https://oauth.reddit.com/api/v1/me", headers=headers)
-
-	#Some debugging
-	print('-------User Data--------\n', r.json(), '\n------------------')
+	print('Exiting script.')
 
 
-	#Let's try making a post to /r/FreeKarma.  You WILL need a little
-	#bit of link karma before your bot can post without getting stopped
-	#by the captcha
-	post_data = {
-				'api_type': 'json',
-				'kind': 'self',
-				'sr' : 'FreeKarma',
-				'title' : 'Hello, world.',
-				'text' : 'Hooray, it works!'
-				}
 
-	r = client.post("https://oauth.reddit.com/api/submit", headers=headers, data=post_data)
-
-	#More debugging
-	print('----------Submission Response----------\n', r.json(), '\n-------------------')
 
 
 if __name__ == '__main__':
